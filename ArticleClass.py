cd761a7002f67d4e67e38a31bc901fb94a9e8fb0
KBlijd <85824981+KBlijd@users.noreply.github.com>
2021-06-13 18:58:13 +0200

    Delete ArticleClass.py

--- ArticleClass.py
+++
@@ -1,6 +0,0 @@
-class Article:
-    def __init__(self, article): # self, code, name, price
-        article = article.split()
-        self.code = article[0]
-        self.name = article[1:-1]
-        self.price = article[-1]
